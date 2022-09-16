f905193275df4607e235263cc98913406f6b3efb
yxx-dev <hermitlun@gmail.com>
2022-09-16 12:11:32 -0700

    post not working yet

--- routes/notes.js
+++ routes/notes.js
@@ -12,7 +12,26 @@ nt.get('/', (req, res) => {
 
 //POST route
 nt.post('/', (req, res) => {
+    //const {title, text} = req.body;
+    //console.log(title, text);
     console.log(`successful POST call`);
+    let tempDb = {};
+    fs.readFile('./db/db.json','utf8', (err,data) => {
+        if(err) {
+            console.error(err);
+        } else {
+            tempDb = JSON.parse(data);
+            console.log(tempDb);
+            console.log(req.body);
+            //tempdb.push(JSON.parse(req.body));
+            //console.log(data);
+        }
+        
+    })
+
+
+    //fs.appendFile('./db/db.json',, err => 
+    //    err ? console.error(err) : console.log('recorded'));
 })
 
 module.exports = nt;
