fae63f0695aba2266d67c2cc64a4e14753e6a108
yxx-dev <hermitlun@gmail.com>
2022-09-16 11:40:43 -0700

    get route done

--- routes/notes.js
+++ routes/notes.js
@@ -5,12 +5,14 @@ const {v4: uuidv4} = require('uuid');
 
 //GET route
 nt.get('/', (req, res) => {
-
+    console.log(`successful GET call`);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => 
+        err ? console.error(err) : res.json(JSON.parse(data)));
 })
 
 //POST route
 nt.post('/', (req, res) => {
-
+    console.log(`successful POST call`);
 })
 
 module.exports = nt;
