0339eefc069f64f3d6164e4a105fab0efdef4388
yxx-dev <hermitlun@gmail.com>
2022-09-16 13:51:43 -0700

    post working

--- routes/notes.js
+++ routes/notes.js
@@ -5,30 +5,47 @@ const {v4: uuidv4} = require('uuid');
 
 //GET route
 nt.get('/', (req, res) => {
-    console.log(`successful GET call`);
+    console.log(`successful ${req.method} call`);
     fs.readFile('./db/db.json', 'utf8', (err, data) => 
         err ? console.error(err) : res.json(JSON.parse(data)));
 })
 
 //POST route
 nt.post('/', (req, res) => {
-    //const {title, text} = req.body;
-    //console.log(title, text);
-    console.log(`successful POST call`);
-    let tempDb = {};
-    fs.readFile('./db/db.json','utf8', (err,data) => {
-        if(err) {
-            console.error(err);
-        } else {
-            tempDb = JSON.parse(data);
-            console.log(tempDb);
-            console.log(req.body);
-            //tempdb.push(JSON.parse(req.body));
-            //console.log(data);
-        }
-        
-    })
+    console.log(`successful ${req.method} call`);
 
+    const {title, text} = req.body;
+    //record request and add unique id
+    if(title && text) {
+        const noteEntry = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+    
+        const response = {
+            status: 'success',
+            body: noteEntry,
+        };
+        
+        let tempDb = [];
+        fs.readFile('./db/db.json','utf8', (err,data) => {
+            if(err) {
+                console.error(err);
+            } else {
+                tempDb = JSON.parse(data);
+                console.log(tempDb);
+                //console.log(req.body);
+                tempDb.push(noteEntry);
+                console.log(tempDb);
+                fs.writeFile('./db/db.json', JSON.stringify(tempDb, null, " "), err =>
+                    err ? res.json(err) : res.json(response)
+                )
+            }
+            
+        })    
+    } else res.json('incomplete entries')
+    
 
     //fs.appendFile('./db/db.json',, err => 
     //    err ? console.error(err) : console.log('recorded'));
--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const app = express();
 
 //middleware to serve the web pages
 app.use(express.static('public'));
+
+//include JSON and urlencoded middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 //define /api route
 app.use('/api',api);
 
-//need use JSON and urlencoded?
 
 //serve the notes.html page
 app.get('/notes', (req, res) => {
